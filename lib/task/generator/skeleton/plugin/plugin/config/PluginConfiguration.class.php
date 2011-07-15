5848bf906f5c623f2b811d90af7f0396256341dc
kphoen <geek63@gmail.com>
2011-07-15 21:05:47 +0200

    Fix: made the tests launchable in a project context
    	modified:   lib/task/generator/skeleton/plugin/plugin/config/PluginConfiguration.class.php
    	modified:   lib/task/generator/skeleton/plugin/project/config/ProjectConfiguration.class.php

--- lib/task/generator/skeleton/plugin/plugin/config/PluginConfiguration.class.php
+++ lib/task/generator/skeleton/plugin/plugin/config/PluginConfiguration.class.php
@@ -2,7 +2,7 @@
 
 /**
  * ##PLUGIN_NAME## configuration.
- * 
+ *
  * @package     ##PLUGIN_NAME##
  * @subpackage  config
  * @author      ##AUTHOR_NAME##
@@ -27,5 +27,49 @@ class ##PLUGIN_NAME##Configuration extends sfPluginConfiguration
           'routing.load_configuration',
           array('##PLUGIN_NAME##Routing', 'listenToRoutingLoadConfigurationEvent'));
     }
+  }
+
+  /**
+   * Listens for the "task.test.filter_test_files" event and adds tests from the current plugin.
+   *
+   * @param  sfEvent $event
+   * @param  array   $files
+   *
+   * @return array An array of files with the appropriate tests from the current plugin merged in
+   */
+  public function filterTestFiles(sfEvent $event, $files)
+  {
+    $task = $event->getSubject();
+
+    if ($task instanceof sfTestAllTask)
+    {
+      $directory = $this->rootDir.'/test';
+      $names = array();
+    }
+    else if ($task instanceof sfTestFunctionalTask)
+    {
+      $directory = $this->rootDir.'/test/functional';
+      $names = $event['arguments']['controller'];
+    }
+    else if ($task instanceof sfTestUnitTask)
+    {
+      $directory = $this->rootDir.'/test/unit';
+      $names = $event['arguments']['name'];
+    }
+
+    if (!count($names))
+    {
+      $names = array('*');
+    }
+
+    foreach ($names as $name)
+    {
+      // we have to limit the max depth because of the symbolic links
+      // in test/fixtures/project/plugins/*
+      $finder = sfFinder::type('file')->follow_link()->maxdepth(1)->name(basename($name).'Test.php');
+      $files = array_merge($files, $finder->in($directory.'/'.dirname($name)));
+    }
+
+    return array_unique($files);
   }
 }
--- lib/task/generator/skeleton/plugin/project/config/ProjectConfiguration.class.php
+++ lib/task/generator/skeleton/plugin/project/config/ProjectConfiguration.class.php
@@ -22,17 +22,68 @@ class ProjectConfiguration extends sfProjectConfiguration
 
     $this->setPluginPath('##PLUGIN_NAME##', dirname(__FILE__).'/../../../..');
 
+    $pluginsDir = sprintf('%s/plugins', sfConfig::get('sf_root_dir'));
+
+    // make sure that the plugins directory exists
+    if (!is_dir($pluginsDir))
+    {
+      $fs = new sfFileSystem();
+      $fs->mkdirs($pluginsDir);
+    }
+
     // make sure that all the generated things will go into the plugin's
     // dir
-    $fs = new sfFilesystem();
-    $fs->symlink(dirname(__FILE__).'/../../../..', 'plugins/##PLUGIN_NAME##');
+    $this->forceSymlink(dirname(__FILE__).'/../../../..', sprintf('%s/plugins/kNewsPlugin', sfConfig::get('sf_root_dir')));
+  }
+
+  public function getPluginPaths()
+  {
+    if (!isset($this->pluginPaths['']))
+    {
+      $pluginPaths = $this->getAllPluginPaths();
+
+      $this->pluginPaths[''] = array();
+      foreach ($this->getPlugins() as $plugin)
+      {
+        if (isset($pluginPaths[$plugin]))
+        {
+          $this->pluginPaths[''][] = $pluginPaths[$plugin];
+        }
+        // plugin not found, try our dependancies directory
+        else if(($path = $this->getDependancy($plugin)))
+        {
+          $plugin_dir = sprintf('%s/plugins/%s', sfConfig::get('sf_root_dir'), $plugin);
+
+          $this->forceSymlink($path, $plugin_dir);
+          $this->pluginPaths[''][] = $plugin_dir;
+        }
+        else
+        {
+          throw new InvalidArgumentException(sprintf('The plugin "%s" does not exist.', $plugin));
+        }
+      }
+    }
+
+    return $this->pluginPaths[''];
+  }
+
+  protected function forceSymlink($origin, $destination)
+  {
+    $fs = new sfFileSystem();
+
+    if (file_exists($destination))
+    {
+      $fs->remove($destination);
+    }
+
+    $fs->symlink($origin, $destination);
   }
 
   protected function guessDependanciesDirectory()
   {
     // if we are not in standelone, this will point to the "parent"
     // symfony project's plugin directory
-    $this->dependancies_dir = dirname(__FILE__).'/../../../../..';
+    $this->dependancies_dir = dirname(__FILE__).'/../../../../../..';
 
     if (!empty($_SERVER['##PLUGIN_NAME##_DEPENDANCIES']))
     {
@@ -55,43 +106,5 @@ class ProjectConfiguration extends sfProjectConfiguration
     $path = sprintf('%s/plugins/%s', $this->dependancies_dir, $plugin);
 
     return is_dir($path) ? $path : false;
-  }
-
-  public function getPluginPaths()
-  {
-    if (!isset($this->pluginPaths['']))
-    {
-      $fs = new sfFilesystem();
-      $pluginPaths = $this->getAllPluginPaths();
-
-      // make sure that the plugins directory exists
-      if (!is_dir('plugins'))
-      {
-        $fs->mkdirs('./plugins');
-      }
-
-      $this->pluginPaths[''] = array();
-      foreach ($this->getPlugins() as $plugin)
-      {
-        if (isset($pluginPaths[$plugin]))
-        {
-          $this->pluginPaths[''][] = $pluginPaths[$plugin];
-        }
-        // plugin not found, try our dependancies directory
-        else if(($path = $this->getDependancy($plugin)))
-        {
-          $plugin_dir = sprintf('plugins/%s', $plugin);
-
-          $fs->symlink($path, $plugin_dir);
-          $this->pluginPaths[''][] = $plugin_dir;
-        }
-        else
-        {
-          throw new InvalidArgumentException(sprintf('The plugin "%s" does not exist.', $plugin));
-        }
-      }
-    }
-
-    return $this->pluginPaths[''];
   }
 }
