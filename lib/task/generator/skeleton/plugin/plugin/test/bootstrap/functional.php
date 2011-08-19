616789d36f58c915ff8b6d29a309e682d0455f11
kphoen <geek63@gmail.com>
2011-08-19 20:02:19 +0200

    Fix: functional bootstrap was incorrect (impossible to launch a test without beeing in a symfony project root)

--- lib/task/generator/skeleton/plugin/plugin/test/bootstrap/functional.php
+++ lib/task/generator/skeleton/plugin/plugin/test/bootstrap/functional.php
@@ -29,6 +29,8 @@ function ##PLUGIN_NAME##_cleanup()
 ##PLUGIN_NAME##_cleanup();
 register_shutdown_function('##PLUGIN_NAME##_cleanup');
 
-require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
+chdir(dirname(__FILE__).'/../fixtures/project');
+
+require_once 'config/ProjectConfiguration.class.php';
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
 sfContext::createInstance($configuration);
